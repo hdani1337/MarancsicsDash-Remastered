62df2568e7534d3a8c58e2da8f460138f23be5ec
Horváth Dániel <admin@hdani1337.hu>
2020-02-01 18:11:45 +0100

    Inisöl kommit

---
+++ desktop/src/hu/hdani1337/marancsicsdash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.hdani1337.marancsicsdash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.hdani1337.marancsicsdash.MarancsicsDash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarancsicsDash(), config);
+	}
+}
