1e627ac1c2fa41b7bc96b73b03c6e159631795f6
Horváth Dániel <admin@hdani1337.hu>
2020-02-08 20:05:10 +0100

    StageInterface + Példa

--- core/src/hu/hdani1337/marancsicsdash/Stage/InfoStage.java
+++ core/src/hu/hdani1337/marancsicsdash/Stage/InfoStage.java
@@ -4,9 +4,47 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.MyStage;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.ResponseViewport;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.StageInterface;
 
-public class InfoStage extends MyStage {
-    public InfoStage(Viewport viewport, MyGame game) {
-        super(viewport, game);
+public class InfoStage extends MyStage implements StageInterface {
+    public InfoStage(MyGame game) {
+        super(new ResponseViewport(900), game);
+        assignment();
+        setSizes();
+        setPositions();
+        addListeners();
+        setZIndexes();
+        addActors();
+    }
+
+    @Override
+    public void assignment() {
+
+    }
+
+    @Override
+    public void setSizes() {
+
+    }
+
+    @Override
+    public void setPositions() {
+
+    }
+
+    @Override
+    public void addListeners() {
+
+    }
+
+    @Override
+    public void setZIndexes() {
+
+    }
+
+    @Override
+    public void addActors() {
+
     }
 }
