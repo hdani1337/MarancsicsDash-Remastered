4c6d55911602f32a3214749a4cf5484acd772b1c
Horváth Dániel <admin@hdani1337.hu>
2020-02-29 18:00:42 +0100

    StageInterface átnevezve IPrettyStage-re, s ezt a PrettyStage implementálja

--- core/src/hu/csanyzeg/master/MyBaseClasses/Scene2D/PrettyStage.java
+++ core/src/hu/csanyzeg/master/MyBaseClasses/Scene2D/PrettyStage.java
@@ -8,10 +8,10 @@ import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
  * @author hdani1337
  * @date 2020.02.29.
  *
- * Ez az absztrakt osztály tulajdonképpen ugyanazt csinálja, mintha a StageInterfacet implementálnánk,
+ * Ez az absztrakt osztály tulajdonképpen csak a StageInterfacet implementálja,
  * de itt van egy alap konstruktorunk, szóval nem kell minden stagenél egyesével végighívogatni az összes voidot
  * **/
-public abstract class PrettyStage extends MyStage{
+public abstract class PrettyStage extends MyStage implements IPrettyStage {
 
     public PrettyStage(Viewport viewport, MyGame game) {
         super(viewport, game);
@@ -31,42 +31,6 @@ public abstract class PrettyStage extends MyStage{
      * **/
     public void beforeInit(){
     }
-
-    /**
-     * Értékek hozzárendelése a változókhoz és objektumokhoz
-     * **/
-    public abstract void assignment();
-
-
-    /**
-     * Actorok, Labelek méreteinek beállítása
-     * **/
-    public abstract void setSizes();
-
-
-    /**
-     * Actorok, Labelek pozícióinak beállítása
-     * **/
-    public abstract void setPositions();
-
-
-    /**
-     * Listenerek hozzáadása az Actorokhoz, Labelekhez
-     * **/
-    public abstract void addListeners();
-
-
-    /**
-     * Actorok, Labelek Z indexeinek beállítása
-     * **/
-    public abstract void setZIndexes();
-
-
-    /**
-     * Actorok, Labelek hozzáadása a Stagehez
-     * **/
-    public abstract void addActors();
-
 
     /**
      * Az értékek beállítása előtt végrehajtandó utasítások
